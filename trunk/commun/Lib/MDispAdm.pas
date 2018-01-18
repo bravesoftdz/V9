cd4409a53f186bde1e2374ebcf33caffc04f7a6c
franckv67 <34346351+franckv67@users.noreply.github.com>
2018-01-18 15:20:26 +0100

    Changement N° Version

--- trunk/commun/Lib/MDispAdm.pas
+++ trunk/commun/Lib/MDispAdm.pas
@@ -490,8 +490,8 @@ begin
 
   Copyright := '© Copyright ' + Apalatys;
   V_PGI.NumVersion := '9.1' ;
-  V_PGI.NumBuild := IntToStr(V_PGI.NumVersionBase) + '.148';
-  V_PGI.DateVersion := EncodeDate(2017, 12, 14) ;
+  V_PGI.NumBuild := IntToStr(V_PGI.NumVersionBase) + '.149';
+  V_PGI.DateVersion := EncodeDate(2018,01,18) ;
 
   V_PGI.LaSerie := S5;
 
